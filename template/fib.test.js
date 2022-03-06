443bdccdf651f41d9eb7d92d56e68e67388ca8a0
khaled hossain <md.khaled.eee@gmail.com>
2022-03-06 14:32:18 +0600

    Add test for template

--- template/fib.test.js
+++ template/fib.test.js
@@ -1,1 +1,13 @@
 const {fibonacci1, fibonacci2} = require('./fib');
+
+const solutions = [fibonacci1, fibonacci2];
+
+for (let solutionNumber = 0; solutionNumber < solutions.length; solutionNumber++) {
+    const solution = solutions[solutionNumber];
+
+    describe(`fib solution${solutionNumber + 1}`, () => {        
+        test('returns empty array', () => {
+            expect(solution()).toEqual([]);
+        });
+    });
+}
